ce5dfb93d61e8807895d2bfa5be67bc60e368c22
Joseph Henrich <jhenrich@constantcontact.com>
2012-11-29 15:26:57 -0500

    Adding rails scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Delve::Application.config.secret_token = 'd31e2a4d8fb1d4849ed47be9f02542600ce9812b6d608ebd3fee500fca6c4bd1d56d620b0de5d698fe31f055162d3ec804a419c582edf61c76e36509c56ece74'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Delve::Application.config.session_store :cookie_store, key: '_delve_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Delve::Application.config.session_store :active_record_store
